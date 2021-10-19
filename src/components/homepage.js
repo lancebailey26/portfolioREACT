113d83169d8791745472fb35e433bdb5f914c4ad
Lance Bailey <lancebailey222@gmail.com>
2021-10-18 22:27:44 -0400

    changes

---
+++ src/components/homepage.js
@@ -0,0 +1,11 @@
+export default function homepage(){
+    
+    return (
+        <div className='center '>
+            <h1>Hi, I'm Lance Bailey.</h1>
+            <br></br>
+            <p>I'm a Full Stack Web Developer.</p>
+            <p>Graduate of The Ohio State University.</p>
+        </div>
+    )
+}
