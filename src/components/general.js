99191c48090be4c67f63045ef30dc526a2d65923
Lance Bailey <lancebailey222@gmail.com>
2021-10-19 21:03:12 -0400

    borpa is funny for the night

--- src/components/general.js
+++ src/components/general.js
@@ -1,9 +1,10 @@
 import cert from '../assets/cert.pdf'
+import borpa from '../assets/621.gif'
 export default function general(){
     return(
         <div className='content center'>
             <p>Here's my <a href={cert} target = "_blank">OSU Certification.</a></p>
-           
+            <img src={borpa}/>
         </div>
     )
 }
