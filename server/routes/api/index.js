1fda7e8643b86aa27d1f83d634b1f770dc3260da
Lance Bailey <lancebailey222@gmail.com>
2021-11-02 20:45:47 -0400

    heroku

--- server/routes/api/index.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-const blogRoutes = require('./blogroutes')
-router.use('/blogs', blogRoutes);
-
-module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const path = require('path');
-const apiRoutes = require('./api');
-
-router.use('/api', apiRoutes);
-
+// const apiRoutes = require('./api');
+const blogRoutes = require('./blogroutes')
+// router.use('/api', apiRoutes);
+router.use('/api', blogRoutes)
 // serve up react front-end in production
 router.use((req, res) => {
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
