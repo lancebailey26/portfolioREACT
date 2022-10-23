589b31c1c23c3fb95e3ca5b07a6b8fdf1f511927
Lance <lancebailey222@gmail.com>
2022-10-23 19:50:10 -0400

    start script fix

--- client/src/route.js
+++ client/src/route.js
@@ -1,7 +1,5 @@
 import React from "react";
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import General from "./components/general.js"
 import Intro from "./components/intro.js"
 import Blog from "./components/blog.js"
 import Navigation from "./components/nav.js"
@@ -15,22 +13,16 @@ export default function Routers(){
                 <Switch>
                     <Route exact path="/">
                         <Intro />
-                        {/* <General/> */}
                         <Github/>
                     </Route>
                     <Route exact path="/blog">
                         <Blog/>
                         
                     </Route>
-                    {/* <Route exact path="/portfolio">
-                    <Github/>
-                 
-                    </Route> */}
                     <Route exact path='/kit'>
                     <Kit/>
                     </Route>
                 </Switch>
-                {/* <Contact/> */}
             </Router>
             
         </main>
