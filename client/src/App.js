5ab5cc4390577940da1b590128faca332dbdcec6
Lance <lancebailey222@gmail.com>
2022-10-23 20:26:38 -0400

    nothing makes sense

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,36 @@ import AOS from 'aos';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'aos/dist/aos.css';
 import './App.css';
-import Routers from './route'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Intro from "./components/intro.js"
+import Blog from "./components/blog.js"
+import Navigation from "./components/nav.js"
+import Github from "./components/github.js";
+import Kit from "./components/kit.js";
 AOS.init();
 
 
 function App() {
   return (
-    <Routers/>
-  );
+    <main>
+    <Navigation/>
+    <Router>
+        <Switch>
+            <Route exact path="/">
+                <Intro />
+                <Github/>
+            </Route>
+            <Route exact path="/blog">
+                <Blog/>
+                
+            </Route>
+            <Route exact path='/kit'>
+            <Kit/>
+            </Route>
+        </Switch>
+    </Router>  
+</main>  );
 }
 
 export default App;
