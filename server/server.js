3665cd9fa8a93771c2caaec826f85dd94cee00eb
Lance <lancebailey222@gmail.com>
2022-10-23 20:41:11 -0400

    ?????

--- server/server.js
+++ server/server.js
@@ -8,7 +8,16 @@ app.use(express.json());
 app.use(require('./routes'));
 // get driver connection
 const dbo = require('./db/connection');
- 
+if (process.env.NODE_ENV === 'production') {
+	// Exprees will serve up production assets
+	app.use(express.static('client/build'));
+  
+	// Express serve up index.html file if it doesn't recognize route
+	const path = require('path');
+	app.get('*', (req, res) => {
+	  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+  }
 app.listen(port, () => {
 	// perform a database connection when server starts
 	dbo.connectToServer(function (err) {
