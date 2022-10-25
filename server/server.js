125865b2a0867c492b5db3ffd15c5fa5714379dc
Lance Bailey <lancebailey@Lances-MacBook-Air.local>
2022-10-24 22:27:58 -0400

    server

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const dbo = require('./db/connection');
 // });
 if (process.env.NODE_ENV === 'production') {
 	// Exprees will serve up production assets
-	app.use(express.static('client/build'));
+	app.use(express.static('../client/build'));
   
 	// Express serve up index.html file if it doesn't recognize route
 	const path = require('path');
